09a1872da6768ffaa82d049890af0e8254374278
pdaly91 <patrickdaly91@gmail.com>
2023-07-24 12:26:34 -0700

    basic file setup

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Home from './screens/Home.jsx';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Home />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
